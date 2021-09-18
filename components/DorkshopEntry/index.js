f54dcbde08260872daca25abffec13209543ff1e
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2021-09-17 20:20:53 -0400

    Add calendar event download links

--- components/DorkshopEntry/index.js
+++ components/DorkshopEntry/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
-import Obfuscate from 'react-obfuscate';
-import Moment from 'react-moment';
+import Obfuscate from 'react-obfuscate'
+import Moment from 'react-moment'
+import moment from 'moment'
 
-import styles from './DorkshopEntry.module.css';
+import { ICalendar, GoogleCalendar } from 'datebook'
+
+
+import styles from './DorkshopEntry.module.css'
 
 const DorkshopEntry = (props) => {
 
@@ -16,6 +20,21 @@ const DorkshopEntry = (props) => {
   const dateFormatEnd = "LT"
 
   const durationInMinutes = info.Duration / 60
+
+  const startDate = new Date( info.Date )
+  const endDate = moment( info.Date  ).add(durationInMinutes, 'minutes').toDate()
+
+  const calendarEventConfig = {
+    title: 'Dorkshop: ' + info.Title,
+    location: info.Place,
+    description: info.Description,
+    start: startDate,
+    end: endDate,
+  }
+
+  const googleCalendarEvent = new GoogleCalendar( calendarEventConfig )
+  const icalendarEvent = new ICalendar( calendarEventConfig )
+
 
   return (
     <div className={classes.join(" ")}>
@@ -30,7 +49,7 @@ const DorkshopEntry = (props) => {
           headers={{
             subject: "Dorkshop: " + info.Title
           }} >
-          <span className={styles.emailButton + " button"}>
+          <span className={styles.emailButton}>
             email
           </span>
         </Obfuscate>
@@ -38,6 +57,15 @@ const DorkshopEntry = (props) => {
 
       <div className={styles.dateInfo}>
         <Moment format={dateFormat}>{info.Date}</Moment> - <Moment format={dateFormatEnd} add={{ minutes: durationInMinutes }}>{info.Date}</Moment> {info.Place ? '— '+info.Place : null}
+
+        <a className={styles.calenderEvent} href={ googleCalendarEvent.render() } target="_blank">
+          Google Calendar
+        </a>
+
+        <span className={styles.calenderEvent} onClick={ () => icalendarEvent.download() }>
+          iCal Event
+        </span>
+
       </div>
 
       <p className={styles.description}>
