e20253d336165f6c260a034f266fce629661058f
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2022-03-28 15:19:14 -0400

    Add start sign to email

--- components/EmailTemplate/index.js
+++ components/EmailTemplate/index.js
@@ -1,129 +1,118 @@
-import React, { useRef } from 'react'
-import { toast, ToastContainer } from 'react-nextjs-toast'
-import Obfuscate from 'react-obfuscate'
-import Moment from 'react-moment'
-import moment from 'moment'
+import React, { useRef } from "react";
+import { toast, ToastContainer } from "react-nextjs-toast";
+import Obfuscate from "react-obfuscate";
+import Moment from "react-moment";
+import moment from "moment";
 
-import styles from './EmailTemplate.module.css'
+import styles from "./EmailTemplate.module.css";
 
 const EmailTemplate = (props) => {
+  const emailContent = useRef(null);
 
-  const emailContent = useRef(null)
+  let classes = [styles.EmailTemplate];
+  classes.push(props.className);
 
-  let classes = [styles.EmailTemplate]
-  classes.push(props.className)
+  let entries = "no dorkshops this week";
 
-  let entries = "no dorkshops this week"
+  const dateFormat = "LLLL";
+  const dateFormatEnd = "LT";
 
-  const dateFormat = "LLLL"
-  const dateFormatEnd = "LT"
+  const filteredEntries = props.data.filter(
+    (d) => moment().diff(d.StartDate, "days") <= 0
+  );
 
-  const filteredEntries = props.data.filter(d => moment().diff(d.StartDate,'days') <= 0  )
+  if (filteredEntries.length > 0) {
+    entries = filteredEntries.map((d, k) => {
+      const info = d;
 
-  if( filteredEntries.length > 0 ){
-    entries = filteredEntries.map( (d,k) =>{
+      return (
+        <div key={"dorkshop-" + k}>
+          <b>{info.Title}</b>
+          <p className={styles.personInfo}>
+            {info.Person} — <Obfuscate email={info.Email} />
+            <br />
+            <Moment format={dateFormat}>{info.StartDate}</Moment> -{" "}
+            <Moment format={dateFormatEnd}>{info.EndDate}</Moment>{" "}
+            {info.Place ? "— " + info.Place : null}
+          </p>
 
-      const info = d
+          {info.Description.split(/\n/).map((line) => (
+            <p className={styles.description}>{line}</p>
+          ))}
 
-      return <div key={"dorkshop-"+k}>
-
-        <b>
-          { info.Title }
-        </b>
-        <p className={styles.personInfo}>
-          { info.Person } — <Obfuscate email={info.Email}/>
-        <br/>
-          <Moment format={dateFormat}>
-            {info.StartDate}
-          </Moment> - <Moment format={dateFormatEnd}>
-            {info.EndDate}
-          </Moment> {info.Place ? '— '+info.Place : null}
-        </p>
-
-        { info.Description.split(/\n/).map(line => <p className={styles.description}>{line}</p>) }
-
-        <p>
-          ———————
-        </p>
-
-      </div>
-    } )
+          <p>———————</p>
+        </div>
+      );
+    });
   }
 
-
   const copyText = () => {
+    console.log(emailContent.current.innerHTML);
 
-    console.log( emailContent.current.innerHTML )
-
-    const blobInput = new Blob([emailContent.current.innerHTML], {type: 'text/html'});
-    const clipboardItemInput = new ClipboardItem({'text/html' : blobInput});
-
+    const blobInput = new Blob([emailContent.current.innerHTML], {
+      type: "text/html",
+    });
+    const clipboardItemInput = new ClipboardItem({ "text/html": blobInput });
 
     // copy code
-    navigator.clipboard.write([clipboardItemInput])
+    navigator.clipboard.write([clipboardItemInput]);
     // navigator.clipboardData.setData("text/plain", str);
 
-
     // toast
-    toast.notify('✅ copied!', {
+    toast.notify("✅ copied!", {
       duration: 2,
       type: "success",
       title: "Success",
-      position: "top"
-    })
-  }
-
+      position: "top",
+    });
+  };
 
   return (
     <div className={classes.join(" ")}>
-
       <div className={styles.EmailHeader}>
-        <span className={styles.EmailHeaderInfo}>To:</span> MFADT<br/>
-        <span className={styles.EmailHeaderInfo}>Subject:</span> Dorkshops this month
-
-        <div onClick={()=>copyText()} className={styles.CopyBtn}>
+        <span className={styles.EmailHeaderInfo}>To:</span> MFADT
+        <br />
+        <span className={styles.EmailHeaderInfo}>Subject:</span> Dorkshops this
+        month
+        <div onClick={() => copyText()} className={styles.CopyBtn}>
           Copy Email
         </div>
-
       </div>
 
       <div ref={emailContent} className={styles.EmailContent}>
+        <p>Hello everyone!</p>
 
-
-        <p>
-          Hello everyone!
-        </p>
-        <p>
-          Here are the next dorkshops:
-        </p>
-
-        <br/>
-
-        <p>
-          ———————
+        <p style={{ color: "#0AEBA5" }}>
+          <span style={{ fontSize: "60px" }}>✹</span>
+          <br />
+          Please look for the star ↑ to find the room
         </p>
 
+        <p>Here are the next dorkshops:</p>
+
+        <br />
+
+        <p>———————</p>
 
         {entries}
 
-        <br/>
+        <br />
 
         <p>
-          We hope there is something in there for you! Also, check the <a href="https://mfadt.parsons.edu/dorkshops/">dorkshop website</a> to see upcoming dorkshops.
+          We hope there is something in there for you! Also, check the{" "}
+          <a href="https://mfadt.parsons.edu/dorkshops/">dorkshop website</a> to
+          see upcoming dorkshops.
         </p>
         <p>
-          Do you want to give a dorkshop? Send us an email or fill out <a href="https://airtable.com/shrGl9Ymp2a4u1iXy">this form</a>.
+          Do you want to give a dorkshop? Send us an email or fill out{" "}
+          <a href="https://airtable.com/shrGl9Ymp2a4u1iXy">this form</a>.
         </p>
-        <p>
-          — The dorkshop team
-        </p>
-
+        <p>— The dorkshop team</p>
       </div>
 
       <ToastContainer align={"center"} position={"top"} />
-
     </div>
-  )
-}
+  );
+};
 
-export default EmailTemplate
+export default EmailTemplate;
--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
@@ -13,17 +13,22 @@ const ftpConfig = {
   // include: ["*", "**/*"],      // this would upload everything except dot files
   include: ["*", "**/*"],
   // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
-  exclude: ["dist/**/*.map", "node_modules/**", "node_modules/**/.*", ".git/**", ".DS_Store"],
+  exclude: [
+    "dist/**/*.map",
+    "node_modules/**",
+    "node_modules/**/.*",
+    ".git/**",
+    ".DS_Store",
+  ],
   // delete ALL existing files at destination before uploading, if true
   deleteRemote: false,
   // Passive mode is forced (EPSV command is not sent)
   forcePasv: true,
   // use sftp or ftp
-  sftp: true
-}
+  sftp: true,
+};
 
 ftpDeploy
   .deploy(ftpConfig)
-  .then(res => console.log("finished:", res))
-  .catch(err => console.log(err));
-
+  .then((res) => console.log("finished:", res))
+  .catch((err) => console.log(err));
